f424b54cca07c308e12f070d49c98b8dc2530745
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-16 19:39:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
-
 include 'config.php';
 include 'functions.php';
-
 if (isset($_POST)) {
 	if(isset($_POST['upload'])) {	
 		$upload = upload(); 
 	}
-
 	if(isset($_POST['delete'])) {
 		$delete = del(FILES_PATH.DS);
 	}
-	if (file_exists(FILES_PATH)) {
+	
+	
+	if (file_exists(FILES_PATH) && (substr(sprintf('%o', fileperms(FILES_PATH)), -4)) == '0777') {
 	$result = files_to_list();
 	} else {
 		$result = false;
